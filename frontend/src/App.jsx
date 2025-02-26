24829a0a272e6f0f0a6de86a49305c64cff7c6b6
anil gummula <anilgummula2060@gmail.com>
2025-02-27 01:30:46 +0530

    frontend updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+import Login from './components/Login'
+import Register from './components/Register'
+import Profile from './components/Profile'
+import AddFriend from './components/AddFriend'
+import Home from './components/Home'
+
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
+    const router = createBrowserRouter([
+        {
+            path : "/",
+            element : <>
+                <Login/>
+            </>
+        },
+        {
+            path : "/register",
+            element : <>
+                <Register/>
+            </>
+        },
+        {
+            path : "/home",
+            element : <>
+                <Home/>
+            </>
+        },
+        {
+            path : "/profile",
+            element : <>
+                <Profile/>
+            </>
+        },
+        {
+            path : "/add",
+            element : <>
+                <AddFriend/>
+            </>
+        },
+    ])
+
+
   return (
-    <div className=''>App</div>
+    <>
+        <RouterProvider router={router} />
+        <ToastContainer/>
+    </>
   )
 }
 
