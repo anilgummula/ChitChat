1b5bf631939fbc13318a72cedd2a4b60c8190f18
anil gummula <anilgummula2060@gmail.com>
2025-03-01 14:18:34 +0530

    updated

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,11 +1,65 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import {Link} from 'react-router-dom'
+import {context} from './Context';
+import { handleError } from '../utils';
 
 const Profile = () => {
+    const API_BASE_URL = import.meta.env.VITE_APP_API_BASE_URL;
+
+    const {logout} = useContext(context);
+    const usermail = localStorage.getItem('loggedInUser');
+    const [userInfo,setUserInfo] = useState([])
+    const [loading,setLoading] = useState(true);
+
+    useEffect(() => {
+
+        const fetchUser = async ()=>{
+            try {
+                const response = await fetch(`${API_BASE_URL}/user/details`,{
+                    method : "POST",
+                    headers : {
+                        authorization : localStorage.getItem("token"),
+                    },
+                    body:JSON.stringify({usermail})
+                });
+
+                const result = await response.json();
+                if(response.ok){
+                    setUserInfo(result);
+                    setLoading(false);
+                    // console.log(result);
+                }
+            } catch (error) {
+                handleError(error);
+            }
+        }
+
+        fetchUser();
+    }, [])
+    
+
   return (
-    <div className='bg-black text-white min-h-screen flex flex-col mx-auto justify-center items-center p-4'>
-        <p className='p-4 text-red-300'>Profile not found!</p>
-        <Link to={'/'} className='text-yellow-300 border p-2 rounded-lg hover:cursor-pointer'>Logout</Link>
+    <div className='bg-black text-white min-h-screen flex flex-col mx-auto items-center space-y-6 mt-24'>
+        {/* <p className='p-4 text-red-300'>Profile not found!</p> */}
+        <div className='flex flex-col mx-auto justify-center text-center space-y-4'>
+
+        {
+            loading? (
+                <div className='flex justify-center m-20 text-green-300 '>
+                    looading loooading looooading!..😴
+                </div>
+            ) : (
+                <>
+                    <img src="/profile-picture.png" className='flex mx-auto h-20 w-20' alt="" />
+                    <p>{userInfo.name}</p>
+                    <p>{userInfo.email}</p>
+                    <p>{userInfo.mobile}</p>
+                </>
+            )
+        }
+        </div>
+
+        <Link to={'/'} onClick={()=>setTimeout(3000,logout())} className='text-yellow-300 border p-2 rounded-lg hover:cursor-pointer mt-12'>Logout</Link>
     </div>
   )
 }
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,38 +1,89 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import React, { useContext, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import { handleError, handleSuccess } from '../utils';
+import { context } from './Context';
 
 const Register = () => {
+    const API_BASE_URL = import.meta.env.VITE_APP_API_BASE_URL;
+
+    const {setUser,setLoggedIn}= useContext(context);
     const [userInfo,setUserInfo] = useState({});
-    const [name,setName] = useState(null);
-    const [email,setEmail] = useState(null);
-    const [mobile,setMobile] = useState(null);
-    const [password,setPassword] = useState(null);
+    const [name,setName] = useState("");
+    const [email,setEmail] = useState("");
+    const [mobile,setMobile] = useState("");
+    const [password,setPassword] = useState("");
 
     const [button,setButton] = useState(true);
+    const navigate = useNavigate();
 
-    const handleSubmit = (e)=>{
+    const handleSubmit = async (e)=>{
         e.preventDefault();
-        if(userInfo){
-            setUserInfo(
-                {
-                    "name":name,
-                    "email":email,
-                    "mobile":mobile,
-                    "password":password
-                }
-            )
+        setButton(false);
+
+        if(!name ||!email  || !mobile || !password){
+            handleError("Fill the form properly bro!");
+            setButton(true);
+            return 
         }
+        try {
+            const url = `${API_BASE_URL}/auth/register`;
+            const response = await fetch(url,{
+                method : "POST",
+                headers : {
+                    'Content-Type' : 'application/json'
+                },
+                body : JSON.stringify({name,email,mobile,password})
+            });
+            const result = await response.json();
+            const {success ,message ,error,jwtToken } = result;
+            if(success){
+                localStorage.setItem('token', jwtToken);
+                localStorage.setItem('loggedInUser', email);
+                
+                setLoggedIn(true);
+                setUserInfo(
+                    {
+                        "name":name,
+                        "email":email,
+                        "mobile":mobile,
+                    }
+                )
+                setUser(userInfo);
+                
+                handleSuccess(message);
+
+                setTimeout(() => {
+                    navigate("/home")
+                }, 1000);
+            }
+            else if(error){
+                const details = error?.details[0].message;
+                handleError(details);
+                setButton(true);
+            }
+            else if(!success){
+                handleError(message);
+                setButton(true);
+            }
+            console.log(result);
+        } catch (err) {
+            handleError(err);
+            setButton(true);
+        }
+          
+
     }
 
   return (
     <>
         <div className='min-h-screen w-full bg-black flex justify-center text-center px-6'>
-            <form action={(e)=>{handleSubmit(e)}} className=' m-auto border-2 border-gray-400 rounded-lg text-gray-300 md:p-10 p-8  items-center space-y-8  '>
+            <form onSubmit={handleSubmit} className=' m-auto border-2 border-gray-400 rounded-lg text-gray-300 md:p-10 p-8 mt-12 md:mt-24  items-center space-y-8  '>
                 <h1>Register now!</h1>
                 <label htmlFor="name">Name</label>
                 <input className='border mx-4 rounded-md h-[32px] px-2 text-slate-400 outline-none border-none bg-white/10'
                     type="text"
                     required 
+                    value={name}
                     onChange={(e)=> {setName(e.target.value)}} 
                 />
                 <br />
@@ -40,6 +91,7 @@ const Register = () => {
                 <input  className='border mx-4 rounded-md h-[32px] px-2 text-slate-400 outline-none border-none bg-white/10'
                     type="text" 
                     required 
+                    value={mobile}
                     onChange={(e)=> {setMobile(e.target.value)}}
                 />
                 <br />
@@ -47,6 +99,7 @@ const Register = () => {
                 <input className='border mx-4 rounded-md h-[32px] px-2 text-slate-400 outline-none border-none bg-white/10'
                     type="text" 
                     required 
+                    value={email}
                     onChange={(e)=> {setEmail(e.target.value)}}
                 />   
                 <br />
@@ -54,6 +107,7 @@ const Register = () => {
                 <input className='border mx-4 rounded-md h-[32px] px-2 text-slate-400 outline-none border-none bg-white/10'
                     type="text" 
                     required 
+                    value={password}
                     onChange={(e)=> {setPassword(e.target.value)}}
                 />
                 <br />
@@ -63,7 +117,7 @@ const Register = () => {
                 <input className={`border bg-white/70 hover:bg-white/60 text-black font-semibold p-1 px-2 rounded-md ${!button ? 'opacity-50 cursor-not-allowed' : ''} flex  mx-auto `}
                     type="submit" 
                     value={button ? "Register" : "Please wait...."} 
-                    onClick={(e)=> setButton(false)} 
+                    // onClick={(e)=> setButton(false)} 
                     disabled={!button}  
                 />   
 
